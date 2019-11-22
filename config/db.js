406e8529e7c4c4909bd8137371c495805914f566
KewinLizarraga <kewin392@gmail.com>
2019-11-22 15:16:02 -0500

    [UPDATE] validation to create accounts

--- config/db.js
+++ config/db.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log('Connected DB') })
     .catch((err) => { console.log('DB connection failed', err) })
 
-// mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// mongoose.connection.on('error', (error) => {
-//     console.log(error);
-// });
-
 // Importing the models
 require('../models/Vacantes');
+require('../models/Usuarios');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { check, sanitizeBody } = require('express-validator');
+
 const homeController = require('../controllers/homeController');
 const vacantesController = require('../controllers/vacantesController');
+const usuariosController = require('../controllers/usuariosController');
 
 const router = express.Router();
 
@@ -13,5 +16,21 @@ router.get('/vacantes/:url', vacantesController.mostrarVacante);
 
 router.get('/vacantes/editar/:url', vacantesController.formEditarVacante);
 router.post('/vacantes/editar/:url', vacantesController.editarVacante);
+
+router.get('/crear-cuenta', usuariosController.formCrearCuenta);
+router.post('/crear-cuenta',
+    [
+        check('nombre').not().isEmpty().withMessage('El nombre es obligatorio.'),
+        check('email').isEmail().withMessage('El email es obligatorio.'),
+        check('password').not().isEmpty().withMessage('El password no puede ir vacio.'),
+        check('confirmar').not().isEmpty().withMessage('Confirmar password, no puede ir vacio.'),
+        sanitizeBody('nombre').escape(),
+        sanitizeBody('email').escape(),
+        sanitizeBody('password').escape(),
+        sanitizeBody('confirmar').escape(),
+    ],
+    usuariosController.validarRegistro,
+    usuariosController.crearCuenta
+);
 
 module.exports = router;
