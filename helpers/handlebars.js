52d2a0b1c54fd185f1c0f814da54a7db78f991e4
KewinLizarraga <kewin392@gmail.com>
2019-11-22 15:18:02 -0500

    [UPDATE] function to clean and display alerts

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -34,12 +34,26 @@ module.exports = {
         skills.forEach(skill => {
             html += `<li ${seleccionadas.includes(skill) ? 'class="activo"' : ''} >${skill}</li>`;
         });
-
         return opciones.fn().html = html;
     },
     tipoContrato: (seleccionado, opciones) => {
         return opciones.fn(this).replace(
             new RegExp(`value="${seleccionado}"`), '$& selected="selected"'
         );
+    },
+    mostrarAlertas: (errores = {}, alertas) => {
+        const categoria = Object.keys(errores);
+
+        let html = '';
+        if (categoria.length) {
+            errores[categoria].forEach(error => {
+                html += `
+                    <div class="${categoria} alerta" >
+                        ${error}
+                    </div>
+                `
+            })
+        }
+        return alertas.fn().html = html;
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const skills = document.querySelector('.lista-conocimientos');
+    let alertas = document.querySelector('.alertas');   // Clean alerts
+
+    if (alertas) limpiarAlertas();
 
     if (skills) {
         skills.addEventListener('click', agregarSkills);
-        // When we are in edit, we call this function
-        skillsSeleccionados();
+        skillsSeleccionados();      // When we are in edit, we call this function
     }
 });
 
@@ -12,12 +14,10 @@ const skills = new Set();
 const agregarSkills = (e) => {
     if (e.target.tagName === 'LI') {
         if (e.target.classList.contains('activo')) {
-            // remove SET and CLASS
-            skills.delete(e.target.textContent);
+            skills.delete(e.target.textContent);    // remove SET and CLASS
             e.target.classList.remove('activo');
         } else {
-            // add el SET y la CLASS
-            skills.add(e.target.textContent);
+            skills.add(e.target.textContent);       // add el SET y la CLASS
             e.target.classList.add('activo');
         }
     }
@@ -30,8 +30,20 @@ const skillsSeleccionados = () => {
 
     seleccionadas.forEach(seleccionada => {
         skills.add(seleccionada.textContent);
-    })
+    });
 
     const skillsArray = [...skills];
     document.querySelector('#skills').value = skillsArray;
+}
+
+const limpiarAlertas = () => {
+    const alertas = document.querySelector('.alertas');
+    const interval = setInterval(() => {
+        if (alertas.children.length > 0) {
+            alertas.removeChild(alertas.children[0]);
+        } else if (alertas.children.length === 0) {
+            alertas.parentElement.removeChild(alertas);
+            clearInterval(interval);
+        }
+    }, 2000);
 }
