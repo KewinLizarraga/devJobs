c3f1e9047f563380fb08db461295bd5799ee8436
KewinLizarraga <kewin392@gmail.com>
2019-11-23 17:35:19 -0500

    [UPDATE] edit profile and validate profile

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,7 +1,5 @@
-const { check, validationResult } = require('express-validator');
-
-
-const Usuario = require('../models/Usuarios');
+const { check, sanitizeBody, validationResult } = require('express-validator');
+const Usuarios = require('../models/Usuarios');
 
 module.exports = {
     formCrearCuenta: (req, res) => {
@@ -32,7 +30,7 @@ module.exports = {
         next();
     },
     crearCuenta: async (req, res, next) => {
-        const usuario = new Usuario(req.body);
+        const usuario = new Usuarios(req.body);
         try {
             await usuario.save();
             res.redirect('/iniciar-sesion');
@@ -46,4 +44,42 @@ module.exports = {
             pageName: 'Iniciar Sesión - DevJobs'
         });
     },
+    formEditarPerfil: (req, res) => {
+        res.render('editar-perfil', {
+            pageName: 'Edita tu perfil en DevJobs',
+            usuario: req.user,
+            cerrarSesion: true,
+            nombre: req.user.nombre,
+        });
+    },
+    editarPerfil: async (req, res, next) => {
+        const usuario = await Usuarios.findById(req.user._id);
+        usuario.nombre = req.body.nombre;
+        usuario.email = req.body.email;
+        if (req.body.password) usuario.password = req.body.password;
+
+        await usuario.save();
+        req.flash('correcto', 'Cambios guardados correctamente.');
+        res.redirect('/administracion');
+    },
+    validarPerfil: (req, res, next) => {
+        if (req.body.password) sanitizeBody('password').escape();
+
+        const errores = validationResult(req);
+
+        if (!errores.isEmpty()) {
+            let err = errores.errors;
+
+            req.flash('error', err.map(error => error.msg));
+            res.render('editar-perfil', {
+                pageName: 'Edita tu perfil en DevJobs',
+                usuario: req.user,
+                cerrarSesion: true,
+                nombre: req.user.nombre,
+                mensajes: req.flash()
+            });
+            return;
+        }
+        next();
+    }
 }
