ebc9e725a0aef14ee0aa77cb0751989b4093da11
KewinLizarraga <kewin392@gmail.com>
2019-11-22 20:39:50 -0500

    [UPDATE] add user verification

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,13 @@ const router = express.Router();
 
 router.get('/', homeController.mostrarTrabajos);
 
-router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante);
-router.post('/vacantes/nueva', vacantesController.agregarVacante);
+router.get('/vacantes/nueva', authController.verificarUsuario, vacantesController.formularioNuevaVacante);
+router.post('/vacantes/nueva', authController.verificarUsuario, vacantesController.agregarVacante);
 
 router.get('/vacantes/:url', vacantesController.mostrarVacante);
 
-router.get('/vacantes/editar/:url', vacantesController.formEditarVacante);
-router.post('/vacantes/editar/:url', vacantesController.editarVacante);
+router.get('/vacantes/editar/:url', authController.verificarUsuario, vacantesController.formEditarVacante);
+router.post('/vacantes/editar/:url', authController.verificarUsuario, vacantesController.editarVacante);
 
 router.get('/crear-cuenta', usuariosController.formCrearCuenta);
 router.post('/crear-cuenta',
@@ -36,5 +36,7 @@ router.post('/crear-cuenta',
 
 router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
 router.post('/iniciar-sesion', authController.autenticarUsuario);
+
+router.get('/administracion', authController.verificarUsuario, authController.mostrarPanel);
 
 module.exports = router;
