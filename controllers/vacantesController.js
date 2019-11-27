251bd678c24e4a206b11ba62152ad5a617e6fb76
KewinLizarraga <kewin392@gmail.com>
2019-11-26 20:56:10 -0500

    [UPDATE] show candidates

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -1,4 +1,6 @@
 const { check, validationResult } = require('express-validator');
+const multer = require('multer');
+const shortid = require('shortid');
 const Vacante = require('../models/Vacantes');
 
 module.exports = {
@@ -85,6 +87,53 @@ module.exports = {
         } else {
             res.status(403).send('Error');
         }
+    },
+    subirCV: (req, res, next) => {
+        upload(req, res, function (error) {
+            if (error) {
+                if (error instanceof multer.MulterError) {
+                    if (error.code === 'LIMIT_FILE_SIZE') req.flash('error', 'El archivo es muy grande: Máximo 300kb');
+                    else req.flash('error', error.message);
+                }
+                else req.flash('error', error.message);
+
+                res.redirect('back');
+                return;
+            } else {
+                return next();
+            }
+        });
+    },
+    contactar: async (req, res, next) => {
+        const vacante = await Vacante.findOne({ url: req.params.url });
+
+        if (!vacante) return next();
+
+        const nuevoCandidato = {
+            nombre: req.body.nombre,
+            email: req.body.email,
+            cv: req.file.filename
+        }
+
+        vacante.candidatos.push(nuevoCandidato);
+        vacante.save();
+        req.flash('correcto', 'Se envió tu curriculum correctamente');
+        res.redirect('/');
+    },
+    mostrarCandidatos: async (req, res, next) => {
+        const vacante = await Vacante.findById(req.params.id);
+        
+        if (vacante.autor != req.user._id.toString()) return next();
+        
+        if (!vacante) return next();
+
+        res.render('candidatos', {
+            pageName: `Candidatos Vacante - ${vacante.titulo}`,
+            cerrarSesion: true,
+            nombre: req.user.nombre,
+            imagen: req.user.imagen,
+            candidatos: vacante.candidatos
+        });
     }
 }
 
@@ -92,3 +141,22 @@ const verificarAutor = (vacante = {}, usuario = {}) => {
     if (!vacante.autor.equals(usuario._id)) return false;
     return true;
 }
+
+const configuracionMulter = {
+    limits: { fileSize: 300000 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, __dirname + '../../public/uploads/cv');
+        },
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`);
+        }
+    }),
+    fileFilter(req, file, cb) {
+        if (file.mimetype === 'application/pdf') cb(null, true);
+        else cb(new Error('Formato no valido'), false);
+    },
+}
+
+const upload = multer(configuracionMulter).single('cv');
